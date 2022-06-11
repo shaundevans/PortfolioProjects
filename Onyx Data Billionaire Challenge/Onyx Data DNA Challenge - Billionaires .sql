c19490f63edf9fbf59c40502f73831ad50fed6ba
Shaun <81536256+shaundevans@users.noreply.github.com>
2022-06-11 12:15:33 -0700

    Add files via upload

--- Onyx Data Billionaire Challenge/Onyx Data DNA Challenge - Billionaires .sql
+++ Onyx Data Billionaire Challenge/Onyx Data DNA Challenge - Billionaires .sql
@@ -30,15 +30,50 @@ or about is null
 
 select * from forbes where "age" is null 
 
--- Many of our null values are due to familial/sibling/spousal holdings.  We won't be able to correct these as a single age unless the individuals are listed 
-select * from forbes 
-where personname ilike any(array['%Beate Heister%', '%Karl Albrect%', '%Hank Meijer%',
-'%Doug Meijer%', '%Doug Meijer%', '%Rober Ng%','%Philip Ng%', '%Hinduja%', '%Tom Love%','%Judy Love%', 
-'%Ian Livingstone%', '%Richard Livingstone%', '%Bajaj%', '%von Finck%', '%resnick%', '%%Safra%', '%John Wilson%', '%Alan Wilson%', 
-'%bruce Wilson%', '%Kwee%','%Yiwen%', '%Bhatia%'])
+select * from forbes where finalworth is null 
+
+select * from forbes where "year" is null 
+
+select * from forbes where "month" is null 
+
+select * from forbes where category is null 
+
+select * from forbes where "source" is null 
+
+select * from forbes where country is null 
+
+select * from forbes where state is null 
+
+select * from forbes where countryofcitizenship is null 
+
+select * from forbes where organization is null 
+
+select * from forbes where selfmade is null 
+
+select * from forbes where gender is null 
+
+select * from forbes where birthdate is null 
+
+select * from forbes where title is null 
+
+-- Leaving Nulls 
+select * from forbes where philanthropyscore is null 
+
+-- Leaving nulls 
+select * from forbes where residencemsa is null 
+
+-- Since there is not way to fix this, this will be left NULL 
+select * from forbes where numberofsiblings is null 
+
+select * from forbes where bio is null 
+
+select * from forbes where about is null 
+
+
 
 -- Updating Age for Individuals will Null Values (given the constraints of our data set, and source material, we'll be using google search to fill in the age nulls in this case)
-/* No Known Age for the following: Zhang Hejun, */ 
+/* No Known Age for the individuals not updated, retained as NULL
+ * Age left Null for multiple parties/families/estates */ 
 update forbes 
 set "age" = (extract(year from current_date) - 1989)
 where personname = 'Maximilian Viessmann'
@@ -46,6 +81,32 @@ where personname = 'Maximilian Viessmann'
 update forbes
 set "age" = (extract(year from current_date) - 1989)
 where personname = 'Zong Yanmin'
+
+update forbes 
+set "age" = 50
+where personname = 'Marcos Galperin'
+
+update forbes 
+set "age" = (extract(year from current_date)- 1949)
+where personname = 'Maren Otto'
+
+update forbes 
+set "age" = 47
+where personname = 'Benjamin Otto'
+
+ 
+update forbes
+set "age" = 69 
+where personname = 'Chen Quiongxiang'
+
+update forbes 
+set "age" = (extract(year from current_date)-1986)
+where personname = 'Christiane Schoeller'
+
+update forbes 
+set "age" = (extract(year from current_date)-1970)
+where personname = 'Melissa Ma'
+
 
 
 /* Billionaire Count by Country */ 
@@ -65,6 +126,11 @@ select stddev(finalworth) as std_dev, round(avg(finalworth)::int4,2) as avg_wort
 from forbes f 
 group by category 
 order by std_dev desc, avg_worth desc 
+
+
+select "rank", personname, "age", finalworth, philanthropyscore from forbes f 
+order by finalworth desc 
+limit 5
 
 
 
