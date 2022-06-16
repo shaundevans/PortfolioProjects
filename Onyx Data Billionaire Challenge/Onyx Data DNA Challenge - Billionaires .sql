64a8ef607ad783890017eaefbd72dec345242b11
Shaun <81536256+shaundevans@users.noreply.github.com>
2022-06-15 21:56:50 -0700

    Add files via upload

--- Onyx Data Billionaire Challenge/Onyx Data DNA Challenge - Billionaires .sql
+++ Onyx Data Billionaire Challenge/Onyx Data DNA Challenge - Billionaires .sql
@@ -172,13 +172,71 @@ order by philanthropyscore desc
 
 
 
+-- Demographics of Billionaires
+select sum(finalworth) as total_wealth, category from forbes f 
+group by category 
+order by total_wealth desc 
+
+
+select avg(finalworth) as total_wealth, avg(philanthropyscore) as avg_phil_score,
+case 
+	when age <18 then '<18'
+	when age >=18 and age <20 then '18-20'
+	when age >=20 and age <30 then '20-30'
+	when age >=30 and age <40 then '30-40'
+	when age >=40 and age <50 then '40-50'
+	when age >=50 and age <60 then '50-60'
+	when age >=60 and age <70 then '60-70'
+	when age >=70 then '>70'
+end as demographics 
+from forbes
+where age is not null 
+and philanthropyscore is not null
+group by demographics 
 
 
 
+select * from forbes
 
-
-
-
+-- Average Philanthropy by Country 
+select avg(finalworth) as total_wealth, avg(philanthropyscore) as avg_phil_score, country from forbes f
+where philanthropyscore is not null 
+group by country
+order by avg_phil_score desc 
  
 
+-- Top Non-US Philanthropists 
+select personname, finalworth, country, philanthropyscore, 
+case 
+	when philanthropyscore = 5 then (finalworth * .20)
+	when philanthropyscore = 4 then (finalworth * .10)
+	when philanthropyscore = 3 then (finalworth * .05)
+	else finalworth * .01
+end as donated_in_bil_low,
+case 
+	when philanthropyscore = 5 then (finalworth * .50)
+	when philanthropyscore = 4 then (finalworth * .1999)
+	when philanthropyscore = 3 then (finalworth * .999)
+	else finalworth * .0499 
+end as donated_in_bil_high
+from forbes 
+where country not ilike '%United States%'
+and philanthropyscore is not null 
 
+
+
+select * from forbes f 
+where countryofcitizenship not ilike '%United STates%'
+order by finalworth desc 
+
+-- Philanthopy by Industry
+
+select avg(philanthropyscore) as avg_phil, count(philanthropyscore) as phil_by_ind, category from forbes f 
+where philanthropyscore is not null 
+group by category 
+order by phil_by_ind desc 
+limit 5
+
+
+select * from forbes 
+where personname ilike any(array['%bill gates%','%warren buffet%','%Mackenzie Scott%','%Elon Musk%','%Len Blavatnik%','Sam Bankman-Fried%','%Blair Parry-Okeden%','%Michael Kim%','%Michael Bloomberg%'])
